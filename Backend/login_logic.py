4f07027f997eee2a691c92a6d7a2d79163be2b19
snehaghule05 <snehaghule2247@gmail.com>
2026-01-25 13:15:46 +0530

    Login logic added

--- Backend/login_logic.py
+++ Backend/login_logic.py
@@ -9,7 +9,7 @@ def validate_email(email):
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
-def register_user(name, email, password):
+def register_user(name, email, password,location):
     if not validate_email(email):
         return {"status":"fail","msg":"Invalid Email"}
     if len(password) < 6:
@@ -17,7 +17,7 @@ def register_user(name, email, password):
     conn = get_connection()
     cur = conn.cursor()
     try:
-        cur.execute("INSERT INTO users(name,email,password) VALUES(%s,%s,%s)",
+        cur.execute("INSERT INTO users(name,email,password,location) VALUES(%s,%s,%s,%s)",
         (name,email,hash_password(password)))
         conn.commit()
         return {"status":"success"}
@@ -29,7 +29,7 @@ def register_user(name, email, password):
 def login_user(email, password):
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("SELECT id FROM users WHERE email=%s AND password=%s",
+    cur.execute("SELECT user_id FROM users WHERE email=%s AND password=%s",
     (email,hash_password(password)))
     user = cur.fetchone()
     conn.close()
