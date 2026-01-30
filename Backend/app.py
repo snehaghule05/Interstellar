d13799816546605621aabdaf59b14ff73f701114
snehaghule05 <snehaghule2247@gmail.com>
2026-01-30 23:12:33 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,20 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from login_logic import register_user, login_user, sessions
+
 import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
-
-# -------------------------
-# DATABASE CONNECTION
-# -------------------------
-def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",          # XAMPP default
-        password="",          # XAMPP default
-        database="interstellar_database"
-    )
 
 # -------------------------
 # SIGNUP API
@@ -27,50 +18,27 @@ def signup():
     password = data.get("password")
     location = data.get("location")
 
-    if not username or not email or not password:
-        return jsonify({"success": False, "message": "Missing fields"}), 400
-
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    # check if email already exists
-    cursor.execute("SELECT user_id FROM users WHERE email = %s", (email,))
-    if cursor.fetchone():
-        return jsonify({"success": False, "message": "Email already registered"}), 409
-
-    # insert new user
-    cursor.execute(
-        "INSERT INTO users (username, email, password, location) VALUES (%s, %s, %s, %s)",
-        (username, email, password, location)
-    )
-    conn.commit()
-
-    cursor.close()
-    conn.close()
-
-    return jsonify({"success": True, "message": "Signup successful"})
+  data = request.json
+    msg = register_user(username,email,password,location)
+    if user:
+        return jsonify({
+            "success": True,
+            "message": "Sign-in successful",
+            "user": user
+        })
+    else:
+        return jsonify({
+            "success": False,
+            "message": "Invalid email or password"
+        }), 401
 
 # -------------------------
 # LOGIN API
 # -------------------------
 @app.route("/login", methods=["POST"])
 def login():
-    data = request.json
-    email = data.get("email")
-    password = data.get("password")
-
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-
-    cursor.execute(
-        "SELECT user_id, username FROM users WHERE email = %s AND password = %s",
-        (email, password)
-    )
-    user = cursor.fetchone()
-
-    cursor.close()
-    conn.close()
-
+   data = request.json
+    result = login_user(email,password)
     if user:
         return jsonify({
             "success": True,
